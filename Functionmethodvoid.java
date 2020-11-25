e8943e727521671c43f65e70c1d2d11a773b65e2
Juansuratno <58240792+Juansuratno@users.noreply.github.com>
2020-11-25 19:40:20 +0800

    Create Functionmethodvoid.java

---
+++ Functionmethodvoid.java
@@ -0,0 +1,23 @@
+public class FunctionMethodvoid {
+    public static void main (String[] args){
+       System.out.println(bangtan());
+       albumm("Part 1");
+       albumm("Part 2");
+       albumm("Young Forever");
+       
+       
+       album("Her"); 
+       album("Tear");
+       album("Answer");
+
+    }
+    private static int bangtan(){
+    return 7;
+    }
+    private static void albumm(String H){
+       System.out.println("The Most Beautiful Moment in Life : " + H); 
+    }
+    private static void album(String LY){
+        System.out.println("Love Yourself : " + LY);
+    }
+}
