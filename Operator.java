cbca5460aca75267f80633c4d4e80bbbd872390c
Juansuratno <58240792+Juansuratno@users.noreply.github.com>
2020-11-25 19:42:08 +0800

    Create Operator.java

---
+++ Operator.java
@@ -0,0 +1,32 @@
+public class Operator {
+    public static void main (String[] args){
+        
+        int a = 10;
+        int b = 4;
+        int hasil;
+         
+       
+  
+        //penjumlahan
+       hasil =(a+b);
+       System.out.println(a + "+" + b + "=" + hasil ); 
+       
+       hasil =(a-b);
+       System.out.println(a + "-" + b + "=" + hasil ); 
+       
+       hasil = (a-b);
+       System.out.printf(" %d - %d  = %d  \n", a,b,hasil );
+       
+       hasil = (a*b);
+       System.out.printf(" %d x %d  = %d  \n", a,b,hasil );
+       
+       
+      
+       float c = (float)a/b;
+       
+       System.out.println(a + " + " + b + " = " + c);
+       
+        
+    }
+    
+}
